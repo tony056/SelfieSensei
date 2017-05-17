ff8231fc1ad80919e908fc6974c81b0facefaa61
Tony Tung <tony61507@gmail.com>
2017-05-17 16:59:53 -0700

    init mobile app

---
+++ Selfie Sensei/Selfie Sensei/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selfie Sensei
+//
+//  Created by Tony Tung on 5/17/17.
+//  Copyright © 2017 Tony Tung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
